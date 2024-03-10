e44a4299c0bbdfecf7f5de2c7982ab0c2543c51a
Cranio290 <143084676+Cranio290@users.noreply.github.com>
2024-03-10 16:49:33 +0100

    Update bot.js

--- chatbot/bot.js
+++ chatbot/bot.js
@@ -1,5 +1,5 @@
-const chatbox = document.getElementsByTagName('input')[0].value;
-const sendbtn =  document.getElementById("sendbtn");
+var chatbox = document.getElementsByTagName('input')[0].value;
+var sendbtn =  document.getElementById("sendbtn");
 
 function sendmsg(txt) {
   chatbox = txt;
